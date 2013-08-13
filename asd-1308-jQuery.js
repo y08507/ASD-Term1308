93c23a53845eb171ac3b5026b1525a47f8be4d16
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-08-12 21:06:23 -0400

    Adding json file

---
+++ asd-1308-jQuery.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Luis Camacho Jr.
+ * Full Sail University
+ * Course: ASD
+ * Term: 1308
+ * Project Week 2
+ * Reference js from previous courses
+ * refactor functions using jquery
+ */
+
+
+$("#projectData").on("pageinit", function(){
+
+
+});
+
---
+++ json.js
@@ -0,0 +1,160 @@
+/*
+ * Author: Luis Camacho Jr.
+ * Full Sail University
+ * Course: ASD
+ * Term: 1308
+ * Project Week 2
+ * json
+ */
+
+
+var json = {
+    "projectData1":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Bioshock"],
+        "newNote":["Enter Note on Project or Idea:", "Complete Bioshock"],
+        "startDate":["Enter Start Date:", "08/12/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData2":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Started reading jQuery for Noobs"],
+        "startDate":["Enter Start Date:", "08/14/13"],
+        "status":["Globalize:", "ExistingClient"]
+    },
+    "projectData3":{
+        "catType":["Choose A Category Type:", "Web Development"],
+        "newID":["Enter New Project or Idea:", "Website"],
+        "newNote":["Enter Note on Project or Idea:", "Working on Website for client 0"],
+        "startDate":["Enter Start Date:", "08/15/13"],
+        "status":["Globalize:", "ExistingClient"]
+    },
+    "projectData4":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Started reading Lost"],
+        "startDate":["Enter Start Date:", "08/15/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData5":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Assassin Creed"],
+        "newNote":["Enter Note on Project or Idea:", "Complete level 1 Assassin Creed"],
+        "startDate":["Enter Start Date:", "08/15/13"],
+        "status":["Globalize:", "ExistingClient"]
+    },
+    "projectData6":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Halo 4"],
+        "newNote":["Enter Note on Project or Idea:", "Started Halo 4 Campaign"],
+        "startDate":["Enter Start Date:", "08/15/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData7":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Started Reading Space 2910"],
+        "startDate":["Enter Start Date:", "08/15/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData8":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Pocket Legends"],
+        "newNote":["Enter Note on Project or Idea:", "Started playing Pocket legends"],
+        "startDate":["Enter Start Date:", "08/21/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData9":{
+        "catType":["Choose A Category Type:", "Technological Creations"],
+        "newID":["Enter New Project or Idea:", "Raspberry Pi"],
+        "newNote":["Enter Note on Project or Idea:", "Working on Raspberry Pi"],
+        "startDate":["Enter Start Date:", "09/01/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData10":{
+        "catType":["Choose A Category Type:", "Web Development"],
+        "newID":["Enter New Project or Idea:", "Website"],
+        "newNote":["Enter Note on Project or Idea:", "Working on web site for Client 1 "],
+        "startDate":["Enter Start Date:", "09/12/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData21":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Assassin Creed"],
+        "newNote":["Enter Note on Project or Idea:", "Started level 2 Assassin Creed"],
+        "startDate":["Enter Start Date:", "09/15/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData11":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Reading The Key Hole"],
+        "startDate":["Enter Start Date:", "09/16/13"],
+        "status":["Globalize:", "NewClient"]
+
+    },
+    "projectData12":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Call of Duty 2"],
+        "newNote":["Enter Note on Project or Idea:", "Playing Call of Duty 2"],
+        "startDate":["Enter Start Date:", "09/17/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData13":{
+        "catType":["Choose A Category Type:", "Technological Creations"],
+        "newID":["Enter New Project or Idea:", "Arduino"],
+        "newNote":["Enter Note on Project or Idea:", "Working on Arduino board"],
+        "startDate":["Enter Start Date:", "09/20/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData14":{
+        "catType":["Choose A Category Type:", "Technological Creations"],
+        "newID":["Enter New Project or Idea:", "Raspberry"],
+        "newNote":["Enter Note on Project or Idea:", "Added wifi to Raspberry Pi"],
+        "startDate":["Enter Start Date:", "09/23/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData15":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Reading A time for Study"],
+        "startDate":["Enter Start Date:", "09/24/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData16":{
+        "catType":["Choose A Category Type:", "Technological Creations"],
+        "newID":["Enter New Project or Idea:", "Arduino"],
+        "newNote":["Enter Note on Project or Idea:", "Adding code to Arduino"],
+        "startDate":["Enter Start Date:", "09/26/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData17":{
+        "catType":["Choose A Category Type:", "Games"],
+        "newID":["Enter New Project or Idea:", "Assassin Creed"],
+        "newNote":["Enter Note on Project or Idea:", "Started Level 3 Assassin Creed"],
+        "startDate":["Enter Start Date:", "09/27/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData18":{
+        "catType":["Choose A Category Type:", "Books"],
+        "newID":["Enter New Project or Idea:", "Reading"],
+        "newNote":["Enter Note on Project or Idea:", "Reading A story of the Ages"],
+        "startDate":["Enter Start Date:", "09/28/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData19":{
+        "catType":["Choose A Category Type:", "Web Development"],
+        "newID":["Enter New Project or Idea:", "Website"],
+        "newNote":["Enter Note on Project or Idea:", "Working with web client 3"],
+        "startDate":["Enter Start Date:", "09/28/13"],
+        "status":["Globalize:", "NewClient"]
+    },
+    "projectData20":{
+        "catType":["Choose A Category Type:", "Web Development"],
+        "newID":["Enter New Project or Idea:", "Website"],
+        "newNote":["Enter Note on Project or Idea:", "working with web client 4"],
+        "startDate":["Enter Start Date:", "09/29/13"],
+        "status":["Globalize:", "ExistingClient"]
+    }
+};
